a70df4af10453a3b83ef90f05c46eda207776bb8
Sky Lin <linsky519@gmail.com>
2021-11-30 08:36:29 -0700

    backend CRUD completed

---
+++ app/controllers/api/pokemons_controller.rb
@@ -0,0 +1,41 @@
+class Api::PokemonsController < ApplicationController
+  before_action :set_pokemon, only: [:show, :update, :destroy]
+  def index
+    render json: Pokemon.all
+  end
+
+  def show
+    render json: @pokemon
+  end
+
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+    if(@pokemon.save)
+      render json: @pokemon
+    else
+      render json: { errors: @pokemon.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if(@pokemon.update(pokemon_params))
+      render json: @pokemon
+    else
+      render json: { errors: @pokemon.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    render json: @pokemon.destroy
+  end
+
+  private
+  
+  def pokemon_params
+    params.require(:pokemon).permit(:name, :location, :move)
+  end
+
+  def set_pokemon
+    @pokemon = Pokemon.find(params[:id])
+  end
+end
