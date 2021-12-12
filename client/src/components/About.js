369debdc1183d8884cc61d7fae72c8bbc6722cc0
Sky Lin <linsky519@gmail.com>
2021-12-11 17:04:24 -0700

    About page done

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,7 +1,8 @@
 const About = () => {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>About Pokemon</h1>
+      <p>The original Pokémon is a role-playing game based around building a small team of monsters to battle other monsters in a quest to become the best. Pokémon are divided into types, such as water and fire, each with different strengths. Battles between them can be likened to the simple hand game rock-paper-scissors. For example, to gain an advantage over a Pokémon that cannot beat an opponent’s Charizard character because of a weakness to fire, a player might substitute a water-based Pokémon. With experience, Pokémon grow stronger, gaining new abilities. By defeating Gym Leaders and obtaining Gym Badges, trainers garner acclaim.</p>
     </div>
   );
 };
